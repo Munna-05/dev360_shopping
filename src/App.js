982057c3f7f15f55733b31806fb1009a5aa26451
munna sajeev <amalp.k@EditPro2022s-MacBook-Pro.local>
2022-12-14 23:46:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Components/Navigation';
+import ProductList from './Components/ProductList';
+import Products from './Components/Products';
+import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
+import Home from './Pages/Home';
+import ProductDetailsPage from './Pages/ProductDetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App ">
+      <BrowserRouter>
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/productDetails/id' element={<ProductDetailsPage/>} />
+      </Routes>
+      </BrowserRouter>
+      
+      
+
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductList from '../Components/ProductList'
+import Products from '../Components/Products'
+
+const Home = () => {
+  return (
+    <div>
+        <ProductList/>
+        <Products/>
+    </div>
+  )
+}
+
+export default Home
